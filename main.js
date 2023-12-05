794b7d14178323e411799f80c43d77d614006807
Matheus Ribeiro dos Santos <matheus.ribeiro@sou.inteli.edu.br>
2023-12-05 15:38:53 -0300

    new commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const html = document.querySelector('html');
+const focoBt = document.querySelector('.app__card-button--foco');
+const curtoBt = document.querySelector('.app__card-button--curto');
+const longoBt = document.querySelector('.app__card-button--longo');
+
+focoBt.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'foco')
+});
+
+curtoBt.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'descanso-curto')
+});
+
+longoBt.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'descanso-longo')
+});
+
+// document.querySelector('button');
+
+// document.querySelectorAll('button');
+
+// document.querySelector('.app__card-button--curto');
+
+// document.getElementsByClassName('app__card-button--curto');
+
+// document.getElementById('start-pause');
