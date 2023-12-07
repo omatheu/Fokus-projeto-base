f395b2616be0f6ad0915b2076e2665317724e66b
Matheus Ribeiro dos Santos <matheus.ribeiro@sou.inteli.edu.br>
2023-12-07 09:35:33 -0300

    adiciona novas features no js

--- main.js
+++ main.js
@@ -1,32 +1,103 @@
-const html = document.querySelector('html');
-const focoBt = document.querySelector('.app__card-button--foco');
-const curtoBt = document.querySelector('.app__card-button--curto');
-const longoBt = document.querySelector('.app__card-button--longo');
-const banner = document.querySelector('.app__image');
+const html = document.querySelector("html");
+const focoBt = document.querySelector(".app__card-button--foco");
+const curtoBt = document.querySelector(".app__card-button--curto");
+const longoBt = document.querySelector(".app__card-button--longo");
+const banner = document.querySelector(".app__image");
+const titulo = document.querySelector(".app__title");
+const botoes = document.querySelectorAll(".app__card-button");
+const startPauseBt = document.querySelector("#start-pause");
+const musicaFocoInput = document.querySelector("#alternar-musica");
+const musica = new Audio("/sons/luna-rise-part-one.mp3");
+const audioPlay = new Audio("/sons/play.wav");
+const audioPausa = new Audio("/sons/pause.mp3");
+const audioTempoFinalizado = new Audio("./sons/beep.mp3");
+const iniciarOuPausarBt = document.querySelector("span");
 
-focoBt.addEventListener('click', () => {
-    alterarContexto('foco');
+const imgBtStartPause = document.querySelector(".app__card-primary-butto-icon");
+
+let tempoDecorridoEmSegundos = 5;
+let intervaloId = null;
+
+musica.loop = true;
+
+musicaFocoInput.addEventListener("change", () => {
+  if (musica.paused) {
+    musica.play();
+  } else {
+    musica.pause();
+  }
 });
 
-curtoBt.addEventListener('click', () => {
-    alterarContexto('descanso-curto');
+focoBt.addEventListener("click", () => {
+  alterarContexto("foco");
+  focoBt.classList.add("active");
 });
 
-longoBt.addEventListener('click', () => {
-    alterarContexto('descanso-longo');
+curtoBt.addEventListener("click", () => {
+  alterarContexto("descanso-curto");
+  curtoBt.classList.add("active");
+});
+
+longoBt.addEventListener("click", () => {
+  alterarContexto("descanso-longo");
+  longoBt.classList.add("active");
 });
 
 function alterarContexto(contexto) {
-    html.setAttribute('data-contexto', contexto)
-    banner.setAttribute('src', `imagens/${contexto}.png`)
+  botoes.forEach(function (contexto) {
+    contexto.classList.remove("active");
+  });
+  html.setAttribute("data-contexto", contexto);
+  banner.setAttribute("src", `/imagens/${contexto}.png`);
+  switch (contexto) {
+    case "foco":
+      titulo.innerHTML = `
+            Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `;
+      break;
+    case "descanso-curto":
+      titulo.innerHTML = `
+            Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>
+            `;
+      break;
+    case "descanso-longo":
+      titulo.innerHTML = `
+            Hora de voltar à superfície.<strong class="app__title-strong"> Faça uma pausa longa.</strong>
+            `;
+    default:
+      break;
+  }
+}
+
+const contagemRegressiva = () => {
+  if (tempoDecorridoEmSegundos <= 0) {
+    audioTempoFinalizado.play();
+    alert("Tempo finalizado!");
+    zerar();
+    return;
+  }
+  tempoDecorridoEmSegundos -= 1;
+  console.log("Temporizador: " + tempoDecorridoEmSegundos);
 };
 
-// document.querySelector('button');
+startPauseBt.addEventListener("click", iniciarOuPausar);
 
-// document.querySelectorAll('button');
+function iniciarOuPausar() {
+  if (intervaloId) {
+    imgBtStartPause.setAttribute("src", "/imagens/play_arrow.png");
+    audioPausa.play();
+    zerar();
+    return;
+  }
+  audioPlay.play();
+  intervaloId = setInterval(contagemRegressiva, 1000);
+  iniciarOuPausarBt.textContent = "Pausar";
+  imgBtStartPause.setAttribute("src", "/imagens/pause.png");
+}
 
-// document.querySelector('.app__card-button--curto');
-
-// document.getElementsByClassName('app__card-button--curto');
-
-// document.getElementById('start-pause');
+function zerar() {
+  clearInterval(intervaloId);
+  iniciarOuPausarBt.textContent = "Começar";
+  intervaloId = null;
+}
